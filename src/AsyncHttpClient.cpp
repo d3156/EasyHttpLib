f03c843b65f56e39b0441ab8c18f89fd7a8e881f
smirnov-yv <smirnov-yv@neo-s.ru>
2026-02-14 12:19:53 +0300

    fix async send for http client. Deleted old sync client

--- src/AsyncHttpClient.cpp
+++ src/AsyncHttpClient.cpp
@@ -10,8 +10,8 @@ namespace d3156
 {
 
     AsyncHttpClient::AsyncHttpClient(net::io_context &ioc, const std::string &host, const std::string &cookie,
-                                     const std::string &authorization)
-        : authorization_(authorization), cookie_(cookie), ioc_(ioc), ssl_ctx_(ssl::context::tlsv13_client)
+                                     const std::string &authorization, size_t max_async_send)
+        : authorization_(authorization), cookie_(cookie), ioc_(ioc), ssl_ctx_(ssl::context::tlsv13_client), max_queued_senders(max_async_send)
     {
         ssl_ctx_.set_default_verify_paths();
         host_clean_ = host;
@@ -53,31 +53,38 @@ namespace d3156
                     beast::system_error er{
                         beast::error_code(static_cast<int>(::ERR_get_error()), net::error::get_ssl_category())};
                     R_LOG(1, "SSL_set_tlsext_host_name error: " << er.what());
+                    is_http_connected = false;
                     co_return false;
                 }
                 co_await stream_->async_handshake(ssl::stream_base::client, net::use_awaitable);
                 G_LOG(1, "Connected with new SSL session to " << host_clean_ << ":" << service_);
+                is_http_connected = true;
             } else {
                 tcp_stream_ = std::make_unique<beast::tcp_stream>(ioc_);
                 co_await tcp_stream_->async_connect(results, net::use_awaitable);
                 tcp_stream_->expires_after(std::chrono::seconds(30));
                 G_LOG(1, "Connected HTTP to " << host_clean_ << ":" << service_);
+                is_http_connected = true;
             }
         } catch (std::exception &e) {
+            is_http_connected = false;
             R_LOG(1, "Reconnect error: " << e.what() << " to " << host_clean_ << ":" << service_);
             co_return false;
         }
         co_return true;
     }
 
-    net::awaitable<resp_dynamic_body> AsyncHttpClient::sendAsync(req_string_body req, std::chrono::milliseconds timeout)
+    net::awaitable<resp_dynamic_body> AsyncHttpClient::sendAsync(req_string_body req, size_t retry,
+                                                                 std::chrono::milliseconds timeout)
     {
-        static std::atomic<bool> busy_{false};
-        size_t spins = 0;
+        if (queued_senders >= max_queued_senders) co_return resp_dynamic_body{http::status::bad_request, 11};
+        ++queued_senders;
+        size_t spins = 0, spin_max = timeout / std::chrono::milliseconds(10);
         while (busy_.exchange(true)) {
-            if (++spins > 100) co_return resp_dynamic_body{http::status::bad_request, 11};
+            if (++spins > spin_max) co_return resp_dynamic_body{http::status::bad_request, 11};
             co_await boost::asio::steady_timer(ioc_, std::chrono::milliseconds(10)).async_wait(net::use_awaitable);
         }
+        --queued_senders;
 
         req.set(http::field::host, host_clean_);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
@@ -108,32 +115,34 @@ namespace d3156
         } catch (const std::exception &e) {
             R_LOG(1, "Async request failed " << req.target() << ": " << e.what());
             busy_ = false;
-            co_return resp_dynamic_body{http::status::bad_request, 11};
+            is_http_connected = false;
+            if (retry == 0) co_return resp_dynamic_body{http::status::bad_request, 11};
         }
+        co_return co_await sendAsync(req, --retry, timeout);
     }
 
-    net::awaitable<resp_dynamic_body> AsyncHttpClient::postAsync(std::string path, std::string body,
+    net::awaitable<resp_dynamic_body> AsyncHttpClient::postAsync(std::string path, std::string body, size_t retry,
                                                                  std::chrono::milliseconds timeout)
     {
         req_string_body r{http::verb::post, basePath_ + path, 11};
         r.body() = std::move(body);
-        co_return co_await sendAsync(std::move(r), timeout);
+        co_return co_await sendAsync(std::move(r), retry, timeout);
     }
 
-    net::awaitable<resp_dynamic_body> AsyncHttpClient::getAsync(std::string path, std::string body,
+    net::awaitable<resp_dynamic_body> AsyncHttpClient::getAsync(std::string path, std::string body, size_t retry,
                                                                 std::chrono::milliseconds timeout)
     {
         req_string_body r{http::verb::get, basePath_ + path, 11};
         r.body() = std::move(body);
-        co_return co_await sendAsync(std::move(r), timeout);
+        co_return co_await sendAsync(std::move(r), retry, timeout);
     }
 
-    net::awaitable<resp_dynamic_body> AsyncHttpClient::patchAsync(std::string path, std::string body,
+    net::awaitable<resp_dynamic_body> AsyncHttpClient::patchAsync(std::string path, std::string body, size_t retry,
                                                                   std::chrono::milliseconds timeout)
     {
         req_string_body r{http::verb::patch, basePath_ + path, 11};
         r.body() = std::move(body);
-        co_return co_await sendAsync(std::move(r), timeout);
+        co_return co_await sendAsync(std::move(r), retry, timeout);
     }
 
     net::awaitable<bool> AsyncHttpClient::ensureConnected()
@@ -166,6 +175,7 @@ namespace d3156
             stream_.reset();
         }
         if (tcp_stream_) tcp_stream_.reset();
+        is_http_connected = false;
         co_return;
     }
 
@@ -173,11 +183,7 @@ namespace d3156
 
     void AsyncHttpClient::setContentType(std::string payload) { payload_type_ = std::move(payload); }
 
-    bool AsyncHttpClient::isConnected()
-    {
-        if (use_ssl_ && stream_) return stream_->next_layer().socket().is_open();
-        return tcp_stream_ && tcp_stream_->socket().is_open();
-    }
+    bool AsyncHttpClient::isConnected() { return is_http_connected; }
 
     AsyncHttpClient::~AsyncHttpClient()
     {
--- src/AsyncHttpClient.hpp
+++ src/AsyncHttpClient.hpp
@@ -8,56 +8,63 @@ namespace beast = boost::beast;
 namespace net   = boost::asio;
 namespace ssl   = boost::asio::ssl;
 
-namespace d3156 {
+namespace d3156
+{
 
-using tcp = boost::asio::ip::tcp;
-using resp_dynamic_body = beast::http::response<beast::http::dynamic_body>;
-using req_string_body   = beast::http::request<beast::http::string_body>;
+    using tcp               = boost::asio::ip::tcp;
+    using resp_dynamic_body = beast::http::response<beast::http::dynamic_body>;
+    using req_string_body   = beast::http::request<beast::http::string_body>;
 
-class AsyncHttpClient {
-public:
-    AsyncHttpClient(net::io_context& ioc, const std::string& host, 
-                   const std::string& cookie = "", 
-                   const std::string& authorization = "");
+    class AsyncHttpClient
+    {
+    public:
+        AsyncHttpClient(net::io_context &ioc, const std::string &host, const std::string &cookie = "",
+                        const std::string &authorization = "", size_t max_queued_senders = 10);
 
-    // Только асинхронные методы
-    net::awaitable<resp_dynamic_body> sendAsync(req_string_body req,
-        std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
+        // Только асинхронные методы
+        net::awaitable<resp_dynamic_body> sendAsync(req_string_body req, size_t retry = 1,
+                                                    std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
 
-    net::awaitable<resp_dynamic_body> postAsync(std::string path, std::string body,
-        std::chrono::milliseconds timeout = std::chrono::milliseconds{500});
+        net::awaitable<resp_dynamic_body> postAsync(std::string path, std::string body, size_t retry = 1,
+                                                    std::chrono::milliseconds timeout = std::chrono::milliseconds{500});
 
-    net::awaitable<resp_dynamic_body> getAsync(std::string path, std::string body,
-        std::chrono::milliseconds timeout = std::chrono::milliseconds{500});
+        net::awaitable<resp_dynamic_body> getAsync(std::string path, std::string body, size_t retry = 1,
+                                                   std::chrono::milliseconds timeout = std::chrono::milliseconds{500});
 
-    net::awaitable<resp_dynamic_body> patchAsync(std::string path, std::string body,
-        std::chrono::milliseconds timeout = std::chrono::milliseconds{500});
+        net::awaitable<resp_dynamic_body> patchAsync(std::string path, std::string body, size_t retry = 1,
+                                                     std::chrono::milliseconds timeout = std::chrono::milliseconds{
+                                                         500});
 
-    void setBasePath(std::string basePath);
-    void setContentType(std::string payload);
-    
-    net::awaitable<bool> ensureConnected();
-    net::awaitable<void> disconnect();
-    
-    bool isConnected();
+        void setBasePath(std::string basePath);
+        void setContentType(std::string payload);
 
-    ~AsyncHttpClient();
+        net::awaitable<bool> ensureConnected();
+        net::awaitable<void> disconnect();
 
-private:
-    bool use_ssl_ = true;
-    std::string payload_type_ = "text/plain; charset=utf-8";
-    std::string authorization_;
-    std::string cookie_;
-    std::unique_ptr<beast::ssl_stream<beast::tcp_stream>> stream_;
-    std::unique_ptr<beast::tcp_stream> tcp_stream_;
-    ssl::context ssl_ctx_;
-    std::string host_clean_;
-    std::string service_;
-    net::io_context& ioc_;
-    bool running_ = true;
-    bool inited = false;
-    std::string basePath_;
-    net::awaitable<bool> reconnectAsync();   
-};
+        bool isConnected();
+
+        ~AsyncHttpClient();
+
+    private:
+        std::atomic<bool> is_http_connected;
+
+        size_t max_queued_senders          = 10;
+        std::atomic<size_t> queued_senders = 0;
+        std::atomic<bool> busy_            = false;
+        bool use_ssl_                      = true;
+        std::string payload_type_          = "text/plain; charset=utf-8";
+        std::string authorization_;
+        std::string cookie_;
+        std::unique_ptr<beast::ssl_stream<beast::tcp_stream>> stream_;
+        std::unique_ptr<beast::tcp_stream> tcp_stream_;
+        ssl::context ssl_ctx_;
+        std::string host_clean_;
+        std::string service_;
+        net::io_context &ioc_;
+        bool running_ = true;
+        bool inited   = false;
+        std::string basePath_;
+        net::awaitable<bool> reconnectAsync();
+    };
 
 } // namespace d3156
